6b420318d0952a8c396d532e4b2bcca02bea35a3
ABStein <abs19stein@gmail.com>
2018-10-23 12:37:41 -0500

    should have the correct recipe instructions

--- recipe.rb
+++ recipe.rb
@@ -1,7 +1,7 @@
 #This is a very simple recipe.
 
 # step 1) Crack some eggs
-# step 2) put in pan
-# step 3) mix eggs with a whisk and a bit of milk
+# step 2) mix eggs with a whisk and a bit of milk
+# step 3) put in pan
 # step 4) cook eggs with chesse and tomato sauce
 # step 5) eat eggs and enjoy!
